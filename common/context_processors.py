648bd6688c7ee59a8a9aeaf79e40ba0764156ed5
yahya <yahyaparayil73@gmail.com>
2023-08-04 09:56:20 +0530

    02/08/2023

---
+++ common/context_processors.py
@@ -0,0 +1,14 @@
+# context_processors.py
+
+from common.models import Customer
+
+
+def customer_name(request):
+    customer_name = None
+    if 'customer' in request.session:
+        customer_id = request.session['customer']
+        try:
+            customer_name = Customer.objects.get(id=customer_id)
+        except Customer.DoesNotExist:
+            pass
+    return {'customer_nm': customer_name}
